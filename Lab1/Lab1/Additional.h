de453b2c16e2f10260e6a64e81489298c57e686d
FazeeIn <113844608+FazeeIn@users.noreply.github.com>
2024-03-03 01:17:51 +0300

    Code refactoring.
    The code was refactored. The structure of the project files has also been changed.

--- Lab1/Lab1/Additional.h
+++ Lab1/Lab1/Additional.h
@@ -1,12 +1,33 @@
 #pragma once
 #include "windows.h"
-char filename[255];
-OPENFILENAMEA ofn;
+#include "stack"
+#include "list"
+using namespace std;
+
 #define TextBufferSize					11000
-char Buffer[TextBufferSize];
+extern OPENFILENAMEA ofn;
+extern char filename[255];
+extern char Buffer[TextBufferSize];
+extern int verticesCount;
+extern int firstVertex;
+extern int secondVertex;
+extern int* cycle;
+extern int cycleLength;
+extern int graph[100][100];
+extern int shortPath[100];
+extern int shortPathLength;
 
 void SaveGraph(LPCSTR path);
 void LoadGraph(LPCSTR path);
 void SetOpenFileNameParams(HWND hWnd);
-void FillTheMatrix(char buffer[], int size);
-
+void PrintVerticies(HWND hWnd, int verticiesCount);
+void JoinVertices(HWND hWnd, int firstVertex, int secondVertex, COLORREF pencolor);
+void FillTheMatrix(char buffer[]);
+void ConnectVertices(HWND hWnd, int matrix[100][100], int verticesCount);
+void ClearGraph();
+void FindPath(int matrix[100][100], int VerticesCount, int firstVertex, int secondVertex);
+bool ContainsInMassive(int vertex, list<int>* cycle);
+list<int> AdjacentVertices(int startVertex, int vertex, list<int>* cycle);
+int* FindCycle();
+void PrintPath(HWND hWnd);
+void PrintCycle(HWND hWnd);
--- Lab1/Lab1/Lab1.h
+++ Lab1/Lab1/Lab1.h
@@ -1,3 +1,22 @@
 ﻿#pragma once
-
 #include "resource.h"
+#define MAX_LOADSTRING 100
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+HWND hEdit1;
+HWND hEdit2;
+HWND hEdit3;
+HWND hEdit4;
+PAINTSTRUCT ps;
+HDC hdc;
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK    WndMdlProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK    WndMdlProc2(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK    WndMdlProc3(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
